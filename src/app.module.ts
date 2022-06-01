385a7c0879bd3ccb0f1aa7b70bc06ed1c87930ae
Chalie Choi <ddd1331@gmail.com>
2022-06-01 23:17:44 +0900

    setting: typeorm migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,71 +5,64 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
-import { User } from './users/entities/user.entity';
-import { Post } from './posts/entities/post.entity';
-import { Comment } from './comments/entities/comment.entity';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import { APP_PIPE } from '@nestjs/core';
 import { CommentsModule } from './comments/comments.module';
-import { Like } from './like/entities/like.entity';
-import { Category } from './common/entities/category.entity';
 import { CommonModule } from './common/common.module';
 import { NewsModule } from './news/news.module';
 import { FilesModule } from './files/files.module';
-import { File } from './files/entities/file.entity';
 import { WingmanModule } from './wingman/wingman.module';
-import { ChildComment } from './comments/entities/child_comment.entity';
 import { LikeModule } from './like/likes.module';
 import { HashtagsModule } from './hashtags/hashtags.module';
-import { Hashtag } from './hashtags/entities/hashtag.entity';
-import { PostHashtag } from './posts/entities/post_hashtag.entity';
-// import { RedisCacheModule } from './cache/cache.module';
-import { RecommendedPost } from './posts/entities/recommended_post.entity';
 import { MatcherModule } from './matcher/matcher.module';
-import { RoomLog } from './matcher/room_log.entity';
-import { Room } from './matcher/room.entity';
-import { Chat } from './matcher/chat.entity';
 import { LocalsModule } from './locals/locals.module';
-import { Review } from './locals/entities/review.entity';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database:
-        process.env.NODE_ENV === 'test'
-          ? process.env.TEST_DATABASE_NAME
-          : process.env.DATABASE_NAME,
-      charset: 'utf8mb4',
-      entities: [
-        User,
-        Post,
-        Comment,
-        Like,
-        Category,
-        File,
-        ChildComment,
-        Hashtag,
-        PostHashtag,
-        RecommendedPost,
-        RoomLog,
-        Room,
-        Chat,
-        Review,
-      ],
-      synchronize: true,
-      dropSchema: process.env.NODE_ENV === 'test',
-      // dropSchema: true,
-      // logging: true,
-      // logging: ['error', 'log'],
-    }),
+    TypeOrmModule.forRoot(),
+    // TypeOrmModule.forRootAsync({
+    //   useFactory: async () =>
+    //     Object.assign(await getConnectionOptions(), {
+    //       autoLoadEntities: true,
+    //       // migrations: [__dirname + '/migrations/**/*.ts'],
+    //     }),
+    // }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: process.env.DATABASE_HOST,
+    //   port: parseInt(process.env.DATABASE_PORT),
+    //   username: process.env.DATABASE_USERNAME,
+    //   password: process.env.DATABASE_PASSWORD,
+    //   database:
+    //     process.env.NODE_ENV === 'test'
+    //       ? process.env.TEST_DATABASE_NAME
+    //       : process.env.DATABASE_NAME,
+    //   charset: 'utf8mb4',
+    //   entities: [
+    //     User,
+    //     Post,
+    //     Comment,
+    //     Like,
+    //     Category,
+    //     File,
+    //     ChildComment,
+    //     Hashtag,
+    //     PostHashtag,
+    //     RecommendedPost,
+    //     RoomLog,
+    //     Room,
+    //     Chat,
+    //     Review,
+    //   ],
+    //   synchronize: true,
+    //   dropSchema: process.env.NODE_ENV === 'test',
+    //   // dropSchema: true,
+    //   // logging: true,
+    //   // logging: ['error', 'log'],
+    // }),
     UsersModule,
     AuthModule,
     PostsModule,
